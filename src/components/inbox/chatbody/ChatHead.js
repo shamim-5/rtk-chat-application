02fa61d60194e382e4f5d8a4a86cfabab07ed405
mss38 <mss38k@gmail.com>
2022-09-27 21:09:27 +0600

    partner avater and name shows on chatHead

--- src/components/inbox/chatbody/ChatHead.js
+++ src/components/inbox/chatbody/ChatHead.js
@@ -1,12 +1,25 @@
-export default function ChatHead({ avatar, name }) {
-    return (
-        <div className="relative flex items-center p-3 border-b border-gray-300">
-            <img
-                className="object-cover w-10 h-10 rounded-full"
-                src={avatar}
-                alt={name}
-            />
-            <span className="block ml-2 font-bold text-gray-600">{name}</span>
-        </div>
-    );
+import { useSelector } from "react-redux";
+import getAvater from "gravatar-url";
+
+export default function ChatHead({ message }) {
+  const { user } = useSelector((state) => state.auth);
+  const { email } = user || {};
+
+  const { sender, receiver } = message || {};
+
+  const partnerEmail = sender.email === email ? receiver : sender.email;
+
+  const partnerName = sender.email === email ? receiver.name : sender.name;
+  console.log("getAvater()");
+
+  return (
+    <div className="relative flex items-center p-3 border-b border-gray-300">
+      <img
+        className="object-cover w-10 h-10 rounded-full"
+        src={getAvater(partnerEmail, { size: 80 })}
+        alt={partnerName}
+      />
+      <span className="block ml-2 font-bold text-gray-600">{partnerName}</span>
+    </div>
+  );
 }
