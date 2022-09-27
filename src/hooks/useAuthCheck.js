3dbc60390a44c22f1b9882536b25ed60b1c511ec
mss38 <mss38k@gmail.com>
2022-09-27 12:17:54 +0600

    login system implemented with json-server-auth

---
+++ src/hooks/useAuthCheck.js
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { userLoggedIn } from "../features/auth/authSlice";
+
+export default function useAuthCheck() {
+  const dispatch = useDispatch();
+  const [authChecked, setAuthChecked] = useState(false);
+
+  useEffect(() => {
+    const localAuth = localStorage?.getItem("auth");
+
+    if (localAuth) {
+      const auth = JSON.parse(localAuth);
+      if (auth?.accessToken && auth?.user) {
+        dispatch(
+          userLoggedIn({
+            accessToken: auth.accessToken,
+            user: auth.user,
+          })
+        );
+      }
+    }
+
+    setAuthChecked(true);
+  }, [dispatch]);
+
+  return authChecked;
+}
