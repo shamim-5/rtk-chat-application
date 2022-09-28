4db95bb3f1daae21a27e22e7cfdeae39f78283dc
mss38 <mss38k@gmail.com>
2022-09-29 04:49:29 +0600

    emitting events from server and client side socket implemented

--- server/server.js
+++ server/server.js
@@ -3,14 +3,29 @@ const jsonServer = require("json-server");
 const express = require("express");
 const http = require("http");
 
-
 const app = express();
-const server = http.createServer(app)
-const io = require('socket.io')(server);
+const server = http.createServer(app);
+const io = require("socket.io")(server);
 
 global.io = io;
 
 const router = jsonServer.router("db.json");
+
+// response middleware
+router.render = (req, res) => {
+  const path = req.path;
+  const method = req.method;
+
+  if (path.includes("/conversations") && (method === "POST" || "PATCH")) {
+    // emit socket event
+    io.emit("conversation", {
+      data: res.locals.data,
+    });
+  }
+
+  res.json(res.locals.data);
+};
+
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 9000;
 
@@ -20,9 +35,9 @@ app.db = router.db;
 app.use(middlewares);
 
 const rules = auth.rewriter({
-    users: 640,
-    conversations: 660,
-    messages: 660,
+  users: 640,
+  conversations: 660,
+  messages: 660,
 });
 
 app.use(rules);
