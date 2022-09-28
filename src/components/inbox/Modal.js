c6b6df255fb865bb4fb9fa86f6f8ec8183e961ff
mss38 <mss38k@gmail.com>
2022-09-28 19:19:50 +0600

    check all requirements for add a new conversation

--- src/components/inbox/Modal.js
+++ src/components/inbox/Modal.js
@@ -1,9 +1,42 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { conversationsApi } from "../../features/conversations/conversationsApi";
+import { useGetUserQuery } from "../../features/users/usersApi";
 import isValidEmail from "../../utils/isValidEmail";
+import Error from "../ui/Error";
 
 export default function Modal({ open, control }) {
   const [to, setTo] = useState("");
   const [message, setMessage] = useState("");
+  const [userCheck, setUserCheck] = useState(false);
+  const { user: loggedInUser } = useSelector((state) => state.auth) || {};
+  const { email: myEmail } = loggedInUser || {};
+  const dispatch = useDispatch();
+  const [responseError, setResponseError] = useState("");
+  const [conversation, setConversation] = useState(undefined);
+
+  const { data: participant } = useGetUserQuery(to, {
+    skip: !userCheck,
+  });
+
+  useEffect(() => {
+    if (participant?.length > 0 && participant[0].email !== myEmail) {
+      // check conversation existance
+      dispatch(
+        conversationsApi.endpoints.getConversation.initiate({
+          userEmail: myEmail,
+          participantEmail: to,
+        })
+      )
+        .unwrap()
+        .then((data) => {
+          setConversation(data);
+        })
+        .catch((err) => setResponseError("There was a problem!"));
+    }
+  }, [participant, myEmail, to, dispatch]);
+    
+    
 
   const debounceHandler = (fn, delay) => {
     let timeoutId;
@@ -18,14 +51,19 @@ export default function Modal({ open, control }) {
   };
 
   const doSearch = (value) => {
-    console.log(value);
     if (isValidEmail(value)) {
-      /// check user API
+      // check user API
+      setUserCheck(true);
       setTo(value);
     }
   };
 
   const handleSearch = debounceHandler(doSearch, 500);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("onsubmit check");
+  };
 
   return (
     open && (
@@ -33,7 +71,8 @@ export default function Modal({ open, control }) {
         <div onClick={control} className="fixed w-full h-full inset-0 z-10 bg-black/50 cursor-pointer"></div>
         <div className="rounded w-[400px] lg:w-[600px] space-y-8 bg-white p-10 absolute top-1/2 left-1/2 z-20 -translate-x-1/2 -translate-y-1/2">
           <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Send message</h2>
-          <form className="mt-8 space-y-6" action="#" method="POST">
+
+          <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
             <input type="hidden" name="remember" value="true" />
             <div className="rounded-md shadow-sm -space-y-px">
               <div>
@@ -71,12 +110,17 @@ export default function Modal({ open, control }) {
               <button
                 type="submit"
                 className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-violet-600 hover:bg-violet-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-violet-500"
+                disabled={conversation === undefined || (participant?.length > 0 && participant[0]?.email === myEmail)}
               >
                 Send Message
               </button>
             </div>
 
-            {/* <Error message="There was an error" /> */}
+            {participant?.length === 0 && <Error message="This user doesn't exist!" />}
+            {participant?.length > 0 && participant[0]?.email === myEmail && (
+              <Error message="You can not send message to yourself" />
+            )}
+            {responseError && <Error message={responseError} />}
           </form>
         </div>
       </>
